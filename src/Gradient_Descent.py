5e7cef31dbf3e20a0f3cf3f956438cac12c93095
Saxsori <saxxxsori@gmail.com>
2024-03-01 22:58:51 +0400

    it doesn't work for the given data set

--- src/Gradient_Descent.py
+++ src/Gradient_Descent.py
@@ -3,15 +3,15 @@ import numpy as np
 from decimal import Decimal
 from Linear_regression import plot_line
 class GradientDescent :
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
-        self.n = len(x)
-        self.learning_rate = Decimal('0.001')
+    def __init__(self, xs: np.ndarray, ys: np.ndarray, lr: Decimal) -> None:
+        self.x = xs
+        self.y = ys
+        self.n = len(xs)
+        self.learning_rate = lr
         self._default_mse = Decimal('1e-40')
     
     @staticmethod
-    def estimate(theta0, theta1: Decimal, x) -> Decimal :
+    def estimate(theta0, theta1: Decimal, x: Decimal) -> Decimal :
         return theta0  + (theta1 * x)
 
     def compute_mse(self, theta0: Decimal, theta1: Decimal) -> Decimal:
@@ -27,7 +27,7 @@ class GradientDescent :
         return (self.learning_rate * tempTheta1) / Decimal(self.n)
     
     def compute_theta0(self, theta0: Decimal, theta1: Decimal) -> Decimal:
-        tempTheta0 = Decimal('0.0')  # Initialize tempTheta0 as a Decimal
+        tempTheta0 = Decimal('0.0')
         for i in range(self.n):
             tempTheta0 += (self.estimate(theta0, theta1, self.x[i]) - self.y[i])
         return (self.learning_rate * tempTheta0) / Decimal(self.n)
@@ -40,33 +40,43 @@ class GradientDescent :
         self._default_mse = default_mse
     
     default_mse = property(get_default_mse, set_default_mse, doc="This is the default_mse")
+    
+    def train_model(self) -> list:
+        cur_mse = Decimal('0.0')
+        theta0 = Decimal('0.0')
+        theta1 = Decimal('0.0')
+        
+        per_mse = None
+        while (1) :
+            theta0 -= self.compute_theta0(theta0, theta1)
+            theta1 -= self.compute_theta1(theta0, theta1)
+            cur_mse = self.compute_mse(theta0, theta1)
+            if per_mse is not None and (per_mse - cur_mse) <= self.default_mse:
+                break
+            per_mse = cur_mse
+            print("MSE change: ", per_mse - cur_mse)
+            print("cur_mse: ", cur_mse)
+            print("per_mse: ", per_mse)
+            print("Theta0: ", theta0)
+            print("Theta1: ", theta1)
+        return [theta0, theta1]
+        
 
-	# def train (self, )
+
 
 def testGradientDescent () :
 	print("Testing Gradient Descent")
-	x = np.array([1, 2, 3, 4, 7, 9, 5]).reshape(-1, 1)
-	y = np.array([2, 3, 5, 7, 7, 99, 11])
-
-	gd = GradientDescent(x, y)
-	print("gd: ", gd)
-	cur_mse = Decimal('0.0')
-	theta0 = Decimal('0.0')
-	theta1 = Decimal('0.0')
+	xs = np.array([1, 2, 3, 4, 7, 9, 5]).reshape(-1, 1)
+	ys = np.array([2, 3, 5, 7, 7, 99, 11])
+ 
+	# print(type(xs))
 	
-	per_mse = None
-	while (1) :
-		theta0 -= gd.compute_theta0(theta0, theta1)
-		theta1 -= gd.compute_theta1(theta0, theta1)
-		cur_mse = gd.compute_mse(theta0, theta1)
-		if per_mse is not None and (per_mse - cur_mse) <= gd.default_mse:
-			print("MSE change: ", per_mse - cur_mse)
-			print("cur_mse: ", cur_mse)
-			print("per_mse: ", per_mse)
-			print("Theta0: ", theta0)
-			print("Theta1: ", theta1)
-			break
-		per_mse = cur_mse
+	
+
+	gd = GradientDescent(xs, ys, Decimal('0.001'))
+	
+	print(gd.train_model())
+	
 
 
 
--- src/train.py
+++ src/train.py
@@ -1,8 +1,25 @@
 from Gradient_Descent import GradientDescent
+from decimal import Decimal
+import numpy as np
+import pandas as pd
 
+from Linear_regression import comput_cofficient
 
 def main () :
-	gd = GradientDescent()
+	data = pd.read_csv("data.csv")
+	print("Testing Gradient Descent")
+	print("km", data["km"].to_numpy())
+	x = data["km"].to_numpy().reshape(-1, 1)
+	y = data["price"].to_numpy()
+	
+	gd = GradientDescent(x, y, Decimal('1.005'))
+	thetas = gd.train_model()
+ 
+	# print("Theta0: ", thetas[0])
+	# print("Theta1: ", thetas[1])
+ 
+	a, b = comput_cofficient(x, y)
+	print("a: ", a, "b: ", b)
 
 
 if __name__ == "__main__" :
