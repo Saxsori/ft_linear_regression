0bf85c9c8fe90444a1fd789e110acd48abf256f2
Saxsori <saxxxsori@gmail.com>
2024-06-23 07:30:50 +0400

    fix after eval

--- launch.sh
+++ launch.sh
@@ -1,31 +1,66 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 {setup|train|precise|predict}"
+	echo "Usage: $0 {setup|train|precise|predict|clean}"
   exit 1
 fi
+
+VENV_DIR="venv"
 
 case $1 in
 	setup)
     	echo "Install Python Packages..."
+		if [ ! -d "$VENV_DIR" ]; then
+            python3 -m venv "$VENV_DIR"
+        fi
+        
+		source "$VENV_DIR/bin/activate"
+
 		pip install --upgrade pip
 		pip install matplotlib numpy pillow pandas    	
     	;;
 	train)
     	echo "Training Started..."
-		(cd src && python train.py)
-    	;;
+        if [ -d "$VENV_DIR" ]; then
+            source "$VENV_DIR/bin/activate"
+            (cd src && python train.py)
+        else
+            echo "Virtual environment not found. Please run '$0 setup' first."
+            exit 1
+        fi    	
+		;;
 	predict)
 		echo "Predict Price..."
-		(cd src && python predict.py)
+        if [ -d "$VENV_DIR" ]; then
+            source "$VENV_DIR/bin/activate"
+            (cd src && python predict.py)
+        else
+            echo "Virtual environment not found. Please run '$0 setup' first."
+            exit 1
+        fi		
 		;;
 	precise)
 		echo "Calculate the precision of my Algorithm..."
-		(cd src && python precision.py)
+        if [ -d "$VENV_DIR" ]; then
+            source "$VENV_DIR/bin/activate"
+            (cd src && python precision.py)
+        else
+            echo "Virtual environment not found. Please run '$0 setup' first."
+            exit 1
+        fi		
 		;;
+	clean)
+        echo "Cleaning up the environment..."
+        if [ -d "$VENV_DIR" ]; then
+            rm -rf "$VENV_DIR"
+            echo "Virtual environment directory deleted."
+        else
+            echo "Virtual environment directory not found."
+        fi
+        ;;
 	*)
 		echo "Invalid option: $1"
-		echo "Usage: $0 {setup|train|precise|predict}"
+		echo "Usage: $0 {setup|train|precise|predict|clean}"
 		exit 1
     	;;
 esac
